d8412ec2fe728c4ea7f425ebcc4282f1e3d05518
tungnt-icd-vn <78336277+tungnt-icd-vn@users.noreply.github.com>
2023-02-17 16:45:07 +0700

    tungn

--- assets/js/common.js
+++ assets/js/common.js
@@ -3,25 +3,26 @@ function addClass(A){document.documentElement.classList.add(A)}var avif=new Imag
 !function(e){"function"==typeof define&&define.amd?define(e):e()}(function(){var e,t=["scroll","wheel","touchstart","touchmove","touchenter","touchend","touchleave","mouseout","mouseleave","mouseup","mousedown","mousemove","mouseenter","mousewheel","mouseover"];if(function(){var e=!1;try{var t=Object.defineProperty({},"passive",{get:function(){e=!0}});window.addEventListener("test",null,t),window.removeEventListener("test",null,t)}catch(e){}return e}()){var n=EventTarget.prototype.addEventListener;e=n,EventTarget.prototype.addEventListener=function(n,o,r){var i,s="object"==typeof r&&null!==r,u=s?r.capture:r;(r=s?function(e){var t=Object.getOwnPropertyDescriptor(e,"passive");return t&&!0!==t.writable&&void 0===t.set?Object.assign({},e):e}(r):{}).passive=void 0!==(i=r.passive)?i:-1!==t.indexOf(n)&&!0,r.capture=void 0!==u&&u,e.call(this,n,o,r)},EventTarget.prototype.addEventListener._original=e}});
 //# Makes {passive: true} by default when EventListenerOptions are supported
 
-// var navicon = document.getElementById('navicon');
-// var navEl = document.getElementById('siteNav');
-//  function toggleMenu(){
-//    navEl.classList.toggle('is_active');
-//    navicon.classList.toggle('is_active');
-// };
-// navicon.addEventListener("click", toggleMenu, false);
-$(function() {
-  $('.m-carousel').not('.slick-initialized').slick({
-    dots: true,
-    speed: 300,
-    arrows: false
-  });
-  $('.m-carousel').on('init', function(){
-    $(this).removeClass('slick-not-init')
-  })
-  /* Simulate defer loading slick */
-  setTimeout(function() {
-    $('.m-carousel').slick({});
-  }, 2500)
-})
 // Load only home page && module slider.pug (pug\_modules\slider.pug)
+jQuery(document).ready(function(){
+  function createSlick(){
+    jQuery(".m-carousel").not('.slick-initialized').slick({
+      dots: true,
+      speed: 300,
+      arrows: false
+    });
+  }
+  function createSupport(){
+    jQuery(".m-support_slick").not('.slick-initialized').slick({
+      dots: true,
+      speed: 300,
+      arrows: true
+    });
+  }
+  createSlick();
+  createSupport();
+
+  //Now it will not throw error
+  $(window).on( 'resize', createSlick);
+  $(window).on( 'resize', createSupport);
+  })
