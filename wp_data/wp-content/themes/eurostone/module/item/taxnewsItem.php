feca0ab8776bfe993889187aec912528ac6c6f37
tungnt-icd-vn <78336277+tungnt-icd-vn@users.noreply.github.com>
2023-03-27 14:25:09 +0700

    tungnt

--- wp_data/wp-content/themes/eurostone/module/item/taxnewsItem.php
+++ wp_data/wp-content/themes/eurostone/module/item/taxnewsItem.php
@@ -1,6 +1,6 @@
 <li class="items">
   <a href="<?php the_permalink()?>">
-    <?php handle_thumbnail('NEWS-THUMBNAIL', true); ?>
+    <?php handle_thumbnail('NEWS-THUMB', true); ?>
     <div class="sec">
       <h4><?php the_title()?></h4>
       <p class="txt"><?php echo wp_trim_words(get_the_excerpt()) ?></p>
