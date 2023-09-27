0a92419a85292953bedd0c7575ffa5fbd9e08ced
tungnt-icd-vn <78336277+tungnt-icd-vn@users.noreply.github.com>
2023-09-27 17:01:46 +0700

    ii

---
+++ wp_data/wp-content/themes/eurostone/taxonomy-colors.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package recruit
+ */
+get_header();
+?>
+<?php
+get_footer();
+
+?>
